ae2096ced89fcaca682eea5178657662fcc60cd7
Kevin Wiliarty <kevin.wiliarty@gmail.com>
2022-02-26 15:32:20 -0500

    sets up Noto Sans

--- main/themes/GlobalStyle.js
+++ main/themes/GlobalStyle.js
@@ -6,7 +6,6 @@ const GlobalStyle = createGlobalStyle`
     font-family: DoulosSIL;
     src: url(static/fonts/DoulosSIL-Regular.woff2) format('woff2'),
          url(static/fonts/DoulosSIL-Regular.woff) format('woff');
-    font-display: block;
   }
 
   /* GentiumPlus */
@@ -15,34 +14,51 @@ const GlobalStyle = createGlobalStyle`
     font-family: GentiumPlus;
     src: url(static/fonts/GentiumPlus-Regular.woff2) format('woff2'),
          url(static/fonts/GentiumPlus-Regular.woff) format('woff');
-    font-display: block;
   }
   @font-face {
     font-family: GentiumPlus;
     src: url(static/fonts/GentiumPlus-Italic.woff2) format('woff2'),
          url(static/fonts/GentiumPlus-Italic.woff) format('woff');
-    font-display: block;
     font-style: italic;
   }
   @font-face {
     font-family: GentiumPlus;
     src: url(static/fonts/GentiumPlus-Bold.woff2) format('woff2'),
          url(static/fonts/GentiumPlus-Bold.woff) format('woff');
-    font-display: block;
     font-weight: bold;
   }
   @font-face {
     font-family: GentiumPlus;
     src: url(static/fonts/GentiumPlus-BoldItalic.woff2) format('woff2'),
          url(static/fonts/GentiumPlus-BoldItalic.woff) format('woff');
-    font-display: block;
+    font-style: italic;
+    font-weight: bold;
+  }
+
+  @font-face {
+    font-family: NotoSans;
+    src: url(static/fonts/NotoSans-Regular.ttf) format('truetype');
+  }
+  @font-face {
+    font-family: NotoSans;
+    src: url(static/fonts/NotoSans-Italic.ttf) format('truetype');
+    font-style: italic;
+  }
+  @font-face {
+    font-family: NotoSans;
+    src: url(static/fonts/NotoSans-Bold.ttf) format('truetype');
+    font-weight: bold;
+  }
+  @font-face {
+    font-family: NotoSans;
+    src: url(static/fonts/NotoSans-BoldItalic.ttf) format('truetype');
     font-style: italic;
     font-weight: bold;
   }
 
   body {
     font-family: ${ props => props.theme.fontFamily };
-    font-family: 'GentiumPlus', sans-serif;
+    font-family: 'NotoSans', sans-serif;
   }
 
   h1 {
