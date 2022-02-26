9588276d1f5ae07af9f9b70cc0f384faecdfd46b
Kevin Wiliarty <kevin.wiliarty@gmail.com>
2022-02-26 11:49:47 -0500

    sets up GentiumPlus fonts

--- main/themes/GlobalStyle.js
+++ main/themes/GlobalStyle.js
@@ -1,15 +1,48 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
-  /* use Doulos SIL - Regular in .woff2 format */
+  /* Doulos SIL - Regular */
   @font-face {
-    font-family: DoulosSILW;
-    src: url(static/fonts/DoulosSIL-Regular.woff2);
+    font-family: DoulosSIL;
+    src: url(static/fonts/DoulosSIL-Regular.woff2) format('woff2'),
+         url(static/fonts/DoulosSIL-Regular.woff) format('woff');
+    font-display: block;
+  }
+
+  /* GentiumPlus */
+  /* Regular */
+  @font-face {
+    font-family: GentiumPlus;
+    src: url(static/fonts/GentiumPlus-Regular.woff2) format('woff2'),
+         url(static/fonts/GentiumPlus-Regular.woff) format('woff');
+    font-display: block;
+  }
+  @font-face {
+    font-family: GentiumPlus;
+    src: url(static/fonts/GentiumPlus-Italic.woff2) format('woff2'),
+         url(static/fonts/GentiumPlus-Italic.woff) format('woff');
+    font-display: block;
+    font-style: italic;
+  }
+  @font-face {
+    font-family: GentiumPlus;
+    src: url(static/fonts/GentiumPlus-Bold.woff2) format('woff2'),
+         url(static/fonts/GentiumPlus-Bold.woff) format('woff');
+    font-display: block;
+    font-weight: bold;
+  }
+  @font-face {
+    font-family: GentiumPlus;
+    src: url(static/fonts/GentiumPlus-BoldItalic.woff2) format('woff2'),
+         url(static/fonts/GentiumPlus-BoldItalic.woff) format('woff');
+    font-display: block;
+    font-style: italic;
+    font-weight: bold;
   }
 
   body {
     font-family: ${ props => props.theme.fontFamily };
-    font-family: 'DoulosSILW', sans-serif;
+    font-family: 'GentiumPlus', sans-serif;
   }
 
   h1 {
