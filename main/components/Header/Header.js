4d2af5fcc84b66087c1c8f2d69fcda14167f69a1
Kevin Wiliarty <kevin.wiliarty@gmail.com>
2022-03-14 21:26:19 -0400

    Header takes props for colors with defaults

--- main/components/Header/Header.js
+++ main/components/Header/Header.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import styled from 'styled-components'
+
+import theme from '../../themes/glyphworks'
 
 import Ipa from '../Ipa'
 
 const StyledHeader = styled.header`
-  background-color: ${ props => props.theme.headerBgColor };
-  color: ${ props => props.theme.headerTextColor };
+  background-color: ${ props => props.backColor };
+  color: ${ props => props.foreColor };
   padding: ${ props => props.theme.basePadding };
   h1 {
     margin: 0;
@@ -14,13 +17,29 @@ const StyledHeader = styled.header`
 `
 
 const Header = props => {
+
+  const backColor = theme.colors[ props.backColor ] || theme.colors[ 'black' ]
+  const foreColor = theme.colors[ props.foreColor ] || theme.colors[ 'white' ]
+
   return (
-    <StyledHeader { ...props }>
+    <StyledHeader backColor={ backColor } foreColor={ foreColor }>
       <h1>
         <Ipa>[ɡlɪfwɜ˞ks]</Ipa>
       </h1>
     </StyledHeader>
   )
+}
+
+Header.propTypes = {
+  /** The background color for the header, the name of a color from the theme */
+  backColor: PropTypes.string,
+  /** The foreground color for the header, the name of a color from the theme */
+  foreColor: PropTypes.string,
+}
+
+Header.defaultProps = {
+  backColor: 'darkRed',
+  foreColor: 'veryLightBrown',
 }
 
 export default Header
--- main/components/Header/Header.md
+++ main/components/Header/Header.md
@@ -1,5 +1,13 @@
 The application Header.
 
+Using the default foreground and background:
+
 ```jsx
 <Header />
+```
+
+But you can also pass in colors as props:
+
+```jsx
+<Header backColor='darkPurple' foreColor='veryLightGreen' />
 ```
