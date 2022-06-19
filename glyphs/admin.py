70cccbaeea805d03893460b5ac1ca0bdaeb7f30e
Kevin Wiliarty <kevin.wiliarty@gmail.com>
2022-06-19 10:12:56 -0400

    Resolves #37: adds glyphs.glyph to site admin

--- glyphs/admin.py
+++ glyphs/admin.py
@@ -1,3 +1,22 @@
-# from django.contrib import admin
+from django.contrib import admin
+from glyphs.models import Glyph
 
-# Register your models here.
+
+class GlyphAdmin(admin.ModelAdmin):
+    list_display = (
+        'glyph',
+        'ipa_definition',
+        'hex_code',
+        'ipa_number',
+        'ipa_name',
+        'slug',
+        'group',
+    )
+    ordering = (
+        'group',
+        'ipa_number',
+        'hex_code',
+    )
+
+
+admin.site.register(Glyph, GlyphAdmin)
