56e202dad93848e8c9d5722c664b1fedc3df1d7f
Kevin Wiliarty <kevin.wiliarty@gmail.com>
2022-06-26 15:45:14 -0400

    Resolves #44: adds graphQL resolver for Glyphs

--- main/schema.py
+++ main/schema.py
@@ -2,10 +2,31 @@
 GraphQL schema
 '''
 import graphene
+from graphene_django import DjangoObjectType
+
+from glyphs.models import Glyph
+
+
+class GlyphType(DjangoObjectType):
+    class Meta:
+        model = Glyph
+        fields = (
+            'glyph',
+            'hex_code',
+            'combining',
+            'ipa_definition',
+            'ipa_number',
+            'ipa_name',
+            'slug',
+            'group',
+        )
 
 
 class Query(graphene.ObjectType):
-    hello = graphene.String(default_value="Hi!")
+    glyphs = graphene.List(GlyphType)
+
+    def resolve_glyphs(root, info):
+        return Glyph.objects.all()
 
 
 schema = graphene.Schema(query=Query)
